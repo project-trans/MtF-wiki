890d8b7e23f0b0c9a1946f2fefc6493ea4cbdff9
Septs <github@septs.pw>
2021-12-25 06:56:39 +0800

    chore: improve render-link

--- assets/customize.js
+++ assets/customize.js
@@ -1,3 +1,13 @@
-document.querySelectorAll('a[href^="mailto:"]').forEach((element) => {
-  element.href = atob(element.href.replace(/^mailto:/, ''));
+document.addEventListener('click', (event) => {
+  const link = event.target.closest('a');
+  if (link === null || link.tagName !== 'A') return;
+  if (link.hostname === location.hostname) return;
+  event.preventDefault();
+  event.stopPropagation();
+  open(link.href);
+});
+
+document.querySelectorAll('a[data-email]').forEach((element) => {
+  element.href = atob(element.dataset.email);
+  delete element.dataset.email;
 });
