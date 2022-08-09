b4cb71ca64e5e257bdbca651c5e0c4dc00acddf7
Septs <github@septs.pw>
2022-08-09 16:37:12 +0800

    feat: add qrcode modal on weixin media account

--- assets/customize.js
+++ assets/customize.js
@@ -8,3 +8,19 @@ document.querySelectorAll("a[data-email]").forEach((element) => {
   element.href = atob(element.dataset.email);
   delete element.dataset.email;
 });
+
+document.addEventListener(
+  "click",
+  (event) => {
+    if (event.target.tagName !== "A") return
+    if (event.target.hostname !== "open.weixin.qq.com") return
+    event.preventDefault();
+    event.stopPropagation();
+    const qrcode = document.createElement("img");
+    qrcode.src = event.target.href;
+    qrcode.width = 430;
+    qrcode.height = 430;
+    swal(event.target.title, { content: qrcode })
+  },
+  { capture: true },
+);
